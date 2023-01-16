769688c98e59ea5d15b9c04c826bbded37c0279a
trba1810 <trba8l@live.com>
2023-01-16 07:10:35 +0100

    added header

---
+++ src/Compontents/Counter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Brojac</h3>
+      </div>
+    );
+  }
+}
+
+export default Counter;
