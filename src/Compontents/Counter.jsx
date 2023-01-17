ff4e87671f0e3946b1f9e83313247f5540f2a724
trba1810 <trba8l@live.com>
2023-01-17 21:49:43 +0100

    initialize state counter

--- src/Compontents/Counter.jsx
+++ src/Compontents/Counter.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleAttack() {
+    alert("Attack clicked");
+  }
+
+  defendeAttack() {
+    alert("Defend clicked");
+  }
+
   render() {
     return (
       <div className="row text-white">
-        <h1>Brojac:</h1>
-        <button style={{ width: "200px" }}>+1</button>
-        <button style={{ width: "200px" }}>-1</button>
+        <h1>Brojac: {this.state.count}</h1>
+        <button onClick={this.handleAttack} style={{ width: "200px" }}>
+          +1
+        </button>
+        <button onClick={this.defendeAttack} style={{ width: "200px" }}>
+          -1
+        </button>
       </div>
     );
   }
